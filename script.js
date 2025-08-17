06ce051277f14595ef138d36ff6602f249af9830
RajashreeSurashe <142881363+RajashreeSurashe@users.noreply.github.com>
2025-08-17 14:12:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Small animation effect on load
+window.addEventListener("load", () => {
+    document.querySelector(".hero h1").style.transition = "1s";
+    document.querySelector(".hero h1").style.transform = "scale(1.1)";
+  });
+  
